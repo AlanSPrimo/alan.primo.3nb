a5312b5d936aec23e8a68f94182bb515d34b86da
AlanSPrimo <166840180+AlanSPrimo@users.noreply.github.com>
2025-02-13 04:11:59 +0000

    Fiz a function publica, mas não consegui exibir os testes mesmo dando OK. Preciso rever o codigo

---
+++ Unidade 1/Aula 1 - Introdução/reverse_string/tests/integration_test.rs
@@ -0,0 +1,9 @@
+use reverse_string::reverse;
+
+fn main() {
+    let my_string = "exemplo";
+    let reversed_string = reverse(my_string);
+    println!("String original: {}", my_string);
+    println!("String invertida: {}", reversed_string);
+
+}
