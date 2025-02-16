e56d819c564140e87e07a7556255cac1284d70e8
AlanSPrimo <166840180+AlanSPrimo@users.noreply.github.com>
2025-02-16 04:06:42 +0000

    Removendo a pasta defeituosa

--- Unidade 1/Aula 1 - Introdução/reverse_string/target/debug/deps/integration_test-9299f5c63db6bd2a.d
+++
@@ -1,5 +0,0 @@
-/workspaces/alan.primo.3nb/Unidade 1/Aula 1 - Introdução/reverse_string/target/debug/deps/integration_test-9299f5c63db6bd2a: tests/integration_test.rs
-
-/workspaces/alan.primo.3nb/Unidade 1/Aula 1 - Introdução/reverse_string/target/debug/deps/integration_test-9299f5c63db6bd2a.d: tests/integration_test.rs
-
-tests/integration_test.rs:
--- Unidade 1/Aula 1 - Introdução/reverse_string/target/debug/deps/reverse_string-ca440d3ef6f02787.d
+++
@@ -1,5 +0,0 @@
-/workspaces/alan.primo.3nb/Unidade 1/Aula 1 - Introdução/reverse_string/target/debug/deps/reverse_string-ca440d3ef6f02787: src/lib.rs
-
-/workspaces/alan.primo.3nb/Unidade 1/Aula 1 - Introdução/reverse_string/target/debug/deps/reverse_string-ca440d3ef6f02787.d: src/lib.rs
-
-src/lib.rs:
